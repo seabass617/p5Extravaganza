f859290907d1f72bd543ced28b0bc51a610633c4
Sebastian Viasus <vseb85@gmail.com>
2020-08-02 20:19:57 -0400

    Bubble Love

--- bubble.js
+++ bubble.js
@@ -1,32 +1,49 @@
-//Here we created the setup for a Bubble class
+//===================================================
+/* Bubble Class */
+//===================================================
 class Bubble { 
-	constructor(x,y,r){
+	/* Constructor instantiating x,y coordinates and radius. Also instansiates default brightness to zero */
+	constructor(x,y,r = 50){
 		this.x = x;
 		this.y = y;
 		this.r = r;
 		this.brightness = 0;
 	}
 
-	clicked(canvas_x, canvas_y){
+	/* Function which will change the color of the brightness of the bubble */
+	changeColor(bright){
+		this.brightness = bright; 
+	}
+
+
+	/* Instance function which returns a bool depending on whether or not 
+	   you are hovering over a bubble */
+	contains(canvas_x, canvas_y){
 		let d = dist(canvas_x, canvas_y, this.x, this.y);
 		if (d < this.r) {
-			this.brightness = 255;
+			return true;
+		} else {
+			return false;
 		}
-
 	}
-	// notice that you don't have to say function in order 
-	//to declare this functionality. Also, note that you 
-	//have to refer to your variable as this.__
+	
+	/* Instance Function which will randomly move the bubble */
 	move(){
 		this.x = this.x + random(-5,5);
 		this.y = this.y + random(-5,5);
 	}
 
+	/* Instance Function which defines the appearance of the bubble */
 	show(){
 		stroke(255);
 		strokeWeight(4);
 		fill(this.brightness, 120);
 		ellipse(this.x,this.y,this.r*2);
 
+	}
+
+	intersects(bubble){
+		let d = dist(this.x, this.y, bubble.x, bubble.y);
+		return d <  this.r + bubble.r;
 	}
 }
--- sketch.js
+++ sketch.js
@@ -1,38 +1,30 @@
-//creating an array to store my bubbles
-let bubbles = []
-//variable that controls the number of bubbles
-let number_of_bubbles = 200;
-let bubble; 
+//============================================
+// Sketch File 
+//============================================
+
+let bubble1; 
+let bubble2;
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  //creating bubble instances with for loop
-  for (let i = 0; i < number_of_bubbles; i++){
-	  let x = random(width);
-	  let y = random(height);
-	  let r = random(10,100);
-	  bubble = new Bubble(x,y,r);
-	  bubbles.push(bubble);
-  }
+	createCanvas(windowWidth,windowHeight);
+	bubble1= new Bubble(200,200,50);
+	bubble2= new Bubble(300,200,50);
+
 }
 
 function draw() {
 	background(0);
-	//applying the functionality of move and show to each bubble
-	
-	for (let i = 0; i < bubbles.length; i++){
-		bubbles[i].move();
-		bubbles[i].show();
-	  }
 
-
-}
-
-function mousePressed(){
-	for (let i = 0; i < bubbles.length; i++){
-	  bubbles[i].clicked(mouseX, mouseY);
+	if (bubble1.intersects(bubble2)) {
+		background(200,0,100);
 	}
+
+	bubble1.show();
+	bubble2.show();
+	bubble1.move();
+	bubble2.move();
 }
+
 
 
 
