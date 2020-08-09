4bf496c1ec2006e62910cc3246d71c24a562efd8
Sebastian Viasus <vseb85@gmail.com>
2020-08-09 18:07:56 -0400

    Spring Motion

--- sketch.js
+++ sketch.js
@@ -2,26 +2,41 @@
 // Sketch File 
 //============================================
 let canvas;
+let pos,target,vel,r,drag,strength,dragSlider,strengthSlider;
 function setup() {
 	canvas = createCanvas(windowWidth,windowHeight);
 	canvas.position(0,0);
 	canvas.style('z-index','-1');
+
+	noStroke();
+	
+	r = 60;
+	position = 0;
+	//target is the position of where the mouse is 
+	target = 0;
+	velocity = 0;
+
+	drag = .8;
+	strength = 0.1;
   }
   
 function draw() {
 	background(0);
-	strokeWeight(2);
-	stroke(255);
-  
-	//first for loop is for the x's
-	for (var x = 0; x <= width; x+=50){
-		//for every x, do the following for every y
-		for (var y = 0; y <= height; y+=50){
-			//then execute this code based on that
-			fill(random(255),0,random(255));
-			ellipse(x,y,25,25);
-		  }
-	  }
+
+	target = mouseX;
+	//position = mouseX;
+
+	let force = target - position;
+	force *= strength;
+	
+	velocity *= drag; 
+	velocity += force;
+	position += velocity; 
+
+	//draw circle 
+	fill(214,71,150);
+	ellipse(position,windowHeight/2,r);
+	
 	   
 }
 
